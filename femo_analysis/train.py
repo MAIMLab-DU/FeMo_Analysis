12076235bc5abfe1b0fe656525d5b42a91e48426
MABatin <leonhsn18@gmail.com>
2024-09-22 09:33:51 +0600

    todo train.py
    Former-commit-id: fc914ff845ed962e3476d9f80d344ab3c5380607

--- femo_analysis/train.py
+++ femo_analysis/train.py
@@ -1,15 +1,48 @@
 # TODO: implement functionality
 import os
 import sys
+import yaml
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
                                              '..', 'src')))
 import argparse
+from logger import LOGGER
+from model.base import FeMoBaseClassifier
+from model import CLASSIFIER_MAP
+
+
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("dataset-dir", type=str, required=True, help="Directory containing train test pickle files")
+    parser.add_argument("dataset-dir", type=str, help="Directory containing train test pickle files")
     parser.add_argument("--work-dir", type=str, default="./work_dir", help="Path to save generated artifacts")
     args = parser.parse_args()
 
     return args
+
+
+def main():
+    args = parse_args()
+
+    config_path = os.path.join(os.path.dirname(__file__), '..', 'configs', 'train-cfg.yaml')
+    with open(config_path, "r") as f:
+        train_cfg = yaml.safe_load(f)
+
+    LOGGER.info(f"Training started with {train_cfg = }")
+    LOGGER.info(f"Working directory {args.work_dir}")
+    
+    try:
+        classifier_type = train_cfg.get('classifier')['type']
+        
+        classifier: type[FeMoBaseClassifier] = CLASSIFIER_MAP[classifier_type]
+        classifier = classifier(**train_cfg.get('config'))
+    except KeyError:
+        LOGGER.error(f"Invalid classifier specified: {train_cfg.get('classifier')}")
+        sys.exit(1)
+
+    classifier.search()
+    
+
+
+if __name__ == "__main__":
+    main()
--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+start_time="$(date +%s)"
+
 # Check if the first argument is provided and store it in DATA_MANIFEST
 if [ -z "$1" ]; then
     echo "Usage: $0 <data_manifest>"
@@ -8,7 +10,7 @@ else
     DATA_MANIFEST="$1"
 fi
 
-# Use the $WORK_DIR environment variable, or default to './work_dir'
+SCRIPT_DIR="femo_analysis"
 WORK_DIR=${WORK_DIR:-"./work_dir"}
 
 # Create the $WORK_DIR directory if it doesn't exist
@@ -30,3 +32,21 @@ echo "Created run directory: $RUN_DIR"
 echo "Data manifest: $DATA_MANIFEST"
 
 # TODO: process.py -> train.py -> evaluate.py pipeline
+VIRTUAL_ENV=.venv
+# Set up virtual env
+virtualenv -p python3.10 $VIRTUAL_ENV
+. $VIRTUAL_ENV/bin/activate
+#Install requirements
+pip install -r ./requirements.txt
+# Run process.py
+# python "$RUN_DIR/process.py" "$DATA_MANIFEST"
+# Run train.py
+python "./$SCRIPT_DIR/train.py" "$DATA_MANIFEST" --work-dir "$RUN_DIR"
+# Run evaluate.py
+# python "$RUN_DIR/evaluate.py"
+
+# Record the end time
+end_time="$(date +%s)"
+# Calculate the running time
+running_time="$((end_time - start_time)) seconds"
+echo "Total time taken: $running_time"
