d30fb6b09bd1d9dc4679f98e088711a98ea79347
MABatin <leonhsn18@gmail.com>
2024-10-23 15:27:46 +0600

    data process for train

--- femo_analysis/process.py
+++ femo_analysis/process.py
@@ -13,8 +13,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("dataManifest", type=str, help="Path to data manifest json file")
     parser.add_argument("--data-dir", type=str, default="./data", help="Path to directory containing .dat and .csv files")
+    parser.add_argument("--params-filename", type=str, default=None, help="Parameters dict filename")
     parser.add_argument("--work-dir", type=str, default="./work_dir", help="Path to save generated artifacts")
-    parser.add_argument("--inference", action='store_true', help="Flag enable data processing for inference")
     parser.add_argument("--extract", action='store_true', default=False, help="Extract features ")
     args = parser.parse_args()
 
@@ -35,20 +35,16 @@ def main():
 
     dataset = FeMoDataset(args.data_dir,
                           args.dataManifest,
-                          args.inference,
+                          False,
                           dataproc_cfg.get('data_pipeline'))
 
     df = dataset.build(force_extract=args.extract)
-    if args.inference:
-        joblib.dump(dataset, os.path.join(args.work_dir, 'inference_dataset.pkl'), compress=True)
-        LOGGER.info("Dataset built for inference.")
-        return
     df.to_csv(os.path.join(args.work_dir, 'features.csv'), header=True, index=False)
     LOGGER.info(f"Features saved to {os.path.abspath(args.work_dir)}")
 
     LOGGER.info("Preprocessing raw input data")
     data_output = dataset.process(input_data=df,
-                                  params_filename=os.path.join(args.work_dir, 'params_dict.pkl'))
+                                  params_filename=args.params_filename)
 
     LOGGER.info(f"Splitting {len(data_output)} rows of data into train, test datasets.")
     split_dict = dataset.split_data(
--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -4,9 +4,6 @@ set -e
 start_time="$(date +%s)"
 SCRIPT_DIR="femo_analysis"
 
-# Initialize CKPT_NAME
-CKPT_NAME=""
-
 # Process the positional arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -20,6 +17,8 @@ while [[ "$#" -gt 0 ]]; do
                 PERF_FILE="$1"
             elif [ -z "$RUN_DIR" ]; then
                 RUN_DIR="$1"
+            elif [ -z "$PARAMS_FILE" ]; then
+                PARAMS_FILE="$1"
             else
                 echo "Unknown option or too many arguments: $1"
                 exit 1
@@ -31,13 +30,14 @@ done
 
 # Check if the required positional arguments are provided
 if [ -z "$DATA_MANIFEST" ] || [ -z "$CKPT_NAME" ]; then
-    echo "Usage: $0 <data_manifest> <ckpt_name> [output_file] [run_dir]"
+    echo "Usage: $0 <data_manifest> <ckpt_name> [output_file] [run_dir] [params_filename]"
     exit 1
 fi
 
 # Set defaults for optional arguments if not provided
 PERF_FILE=${PERF_FILE:-"performance.csv"}
 WORK_DIR=${WORK_DIR:-"./work_dir"}
+PARAMS_FILE=${PARAMS_FILE:-null}
 
 # Create the $WORK_DIR directory if it doesn't exist
 mkdir -p "$WORK_DIR"
@@ -69,13 +69,13 @@ virtualenv -p python3.10 $VIRTUAL_ENV
 pip install -r ./requirements.txt -q
 
 # Run process.py
-python "./$SCRIPT_DIR/process.py" "$DATA_MANIFEST" --work-dir "$RUN_DIR" --data-dir "./data"
+python "./$SCRIPT_DIR/process.py" "$DATA_MANIFEST" --work-dir "$RUN_DIR" --data-dir "./data" --params-filename "$PARAMS_FILE"
 
 # Run train.py
 python "./$SCRIPT_DIR/train.py" "$RUN_DIR" "$CKPT_NAME" --work-dir "$RUN_DIR" --tune
 
 # Run evaluate.py
-python "./$SCRIPT_DIR/evaluate.py" "$DATA_MANIFEST" "$RUN_DIR" --work-dir "$RUN_DIR" --outfile "$PERF_FILE" --ckpt-name "$CKPT_NAME"
+python "./$SCRIPT_DIR/evaluate.py" "$DATA_MANIFEST" "$RUN_DIR" --work-dir "$RUN_DIR" --outfile "$PERF_FILE"
 
 # Record the end time
 end_time="$(date +%s)"
