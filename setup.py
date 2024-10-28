d36fa47f9b7c4bdaef0dba4dcba9d68209a2e979
MABatin <leonhsn18@gmail.com>
2024-10-28 17:16:25 +0600

    bump to version 0.4.0

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import os
 from setuptools import setup, find_packages
+
+def get_version():
+    version_file = os.path.join("femo", "__version__.py")
+    with open(version_file) as f:
+        # Define a dictionary to execute the file contents and fetch __version__
+        version_vars = {}
+        exec(f.read(), version_vars)
+        return version_vars["__version__"]
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
     name="femo",                        # Replace with your package name
-    version="0.3.1",                          # Initial release version
+    version=get_version(),                          # Initial release version
     description="Package for analysis of Fetal Movement data",
     long_description=open("README.md").read(), # Use README.md for the long description
     long_description_content_type="text/markdown",
