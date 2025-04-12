Release v1.1.0: Quaternion normalization and prediction fixes (#134)
* add quaternion normalization step

* fix division by zero issue

* fix: use deepcopy for loaded_data

* fix: handle empty input for model prediction

* chore: bump version to 1.1.0

---------

Co-authored-by: Colin Boyle <colin.boyle@ucd.ie>

femo/data/transforms/preprocess.py
@@ -1,4 +1,5 @@
 import time
+import copy
 import numpy as np
 from scipy.signal import butter, sosfiltfilt
 from ._utils import apply_pca
@@ -47,7 +48,7 @@ class DataPreprocessor(BaseTransform):
     
     def transform(self, loaded_data: dict):
         start = time.time()
-        preprocessed_data = loaded_data.copy()
+        preprocessed_data = copy.deepcopy(loaded_data)
         
         # ---------------------------- Filter design -----------------------------#
         # 3 types of filters are designed-

femo/inference/inference.py
@@ -258,7 +258,10 @@ class PredictionService(object):
             X_norm_ranked = np.hstack([x for x in feature_dict.values()])
             
             clf = self.get_model()
-            y_pred = clf.predict(X_norm_ranked)
+            if X_norm_ranked.shape[0] == 0:
+                y_pred = np.array([0])
+            else:
+                y_pred = clf.predict(X_norm_ranked)
 
             data, ml_map = self._pre_hiccup_removal(
                 filename=file_path,